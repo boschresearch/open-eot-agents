8be670d3d01b4353bc9e83e57915e7beba9bc0ef
danielksan81 <daniel.kunz@de.bosch.com>
2022-03-15 14:51:29 +0100

    Setup Github Workflow Action
    Created workflow file to setup and start ganache to test smart contracts.
    Updated ganache command by removing deterministic option for new
    version.
    Add workflow to setup environment and build included aea.
    Python checks for implementation are included but currently disabled, will be done in next step.
    
    Signed-off-by: Daniel Kunz <daniel.kunz@de.bosch.com>

--- service-directory/start-ganache-aea.sh
+++ service-directory/start-ganache-aea.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-ganache-cli -a 4 -p 8545 -i 1111 --deterministic --mnemonic="project gas foot caught supreme sad bicycle rich act state advance clerk"
+ganache-cli -a 4 -p 8545 -i 1111 --mnemonic="project gas foot caught supreme sad bicycle rich act state advance clerk"
