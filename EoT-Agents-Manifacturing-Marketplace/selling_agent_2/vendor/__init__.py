85564192eda879d546d8f7f56a20ff3b3877a8ce
Daniel Kunz <daniel.kunz@de.bosch.com>
2022-05-18 14:34:14 +0200

    Deletion of duplicated code
    Selling_agent_2 and purchasing_agent_2 are containing the same code as
    the corresponding agents with number 1. To maintain the codebase more
    efficient, they will be deleted.
    
    Signed-off-by: Daniel Kunz <daniel.kunz@de.bosch.com>

--- EoT-Agents-Manifacturing-Marketplace/selling_agent_2/vendor/__init__.py
+++
@@ -1,3 +0,0 @@
-# Copyright (c) 2021 - for information on the respective copyright owner see the NOTICE file and/or the repository https://github.com/boschresearch/open-eot-agents.
-#
-# SPDX-License-Identifier: Apache-2.0
